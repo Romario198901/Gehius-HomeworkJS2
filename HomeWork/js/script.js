67ffffe34b69bc5d599ae35e782b14d1ccf5fd43
Romario198901 <153761577+Romario198901@users.noreply.github.com>
2024-05-31 18:47:36 +0300

    Add files via upload

---
+++ HomeWork/js/script.js
@@ -0,0 +1,140 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+//let value = 1;
+//let value = 0;
+//let value = -3;
+//if (value > 0) {
+  // console.log(true);
+//} 
+//else {
+ //   console.log(false);
+//}
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+//let string;
+//const expression = "test";//
+//switch (expression) {
+   // case true:
+       // string = false;
+      //  break;
+      //  case 'qwerty':
+     //   string = false;
+    //    break;
+//case 'test':
+   // string = true;
+  //  break;
+  //  default: console.log('Try later');
+   // break;
+//}
+//console.log(string);
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+//let number = 1;
+//let number = 10;
+//let number = 13:
+//if (number < 10) {
+//    console.log(number + 5);
+//} 
+//else if(number > 10) { 
+//    console.log (number - 5);
+//}
+//else  {
+ //   console.log(10);
+//}
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+//const subject = prompt("Enter number from" + 1 + " to" + 12);
+//console.log(subject);
+//let month;
+//switch (subject) {
+  //  case "1":
+  //      month = "January";
+     //   alert(month);
+      //  break;
+      //  case "2":
+      //  month = "February";
+      //  alert(month);
+      //  break;
+      //  case "3":
+      //  month = "March";
+      //  alert(month);
+      //  break;
+      //  case "4":
+      //  month = "April";
+      //  alert(month);
+      //  break;
+       // case "5":
+       // month = "May";
+      //  alert(month);
+      //  break;
+      //  case "6":
+       //  month = "June";
+       //  alert(month);
+       // break;
+       // case "7":
+       // month = "July";
+       // alert(month);
+       // break;
+      //  case "8":
+       // month = "August";
+       // break;
+      //  case "9":
+      //      month = "September";
+     //       alert(month);
+     //   break;
+      //  case "10":
+     //   month = "October";
+      //  alert(month);
+    //   break;
+     //   case "11":
+     //   month = "November";
+     //   alert(month);
+     //   break;
+      //  case "12":
+      //  month = "December";
+      //  alert(month);
+       // break;
+   // default: alert("Incorrect value");
+   //     break;
+//}
+
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+//const sumInput = prompt("Enter 3 numbers separated by commas (e.g., 1, 2, 3):");
+//const numbers = sumInput.split(",");
+
+//let sum = 0;
+//for (let i = 0; i < numbers.length; i++) {
+   // switch (i) {
+    //    case 0:
+     //       sum += parseInt(numbers[i]);
+     //       break;
+    //    case 1:
+    //        sum += parseInt(numbers[i]);
+    //        break;
+     //   case 2:
+     //       sum += parseInt(numbers[i]);
+     //       break;
+    //    default:
+     //       break;
+ //   }
+//}
+
+//alert("The sum of the numbers is: " + sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
